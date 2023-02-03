3a4dcc5c5aa8192eb4f6082307aa721d1b029220
Entarudin <riazanov@sfedu.ru>
2023-02-03 15:23:29 +0300

    added realization header

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,17 +1,93 @@
-import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
+import React, { FC, useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 import { ROUTES } from 'shared/constants'
 
+import LoginIcon from 'assets/images/Header/LoginIcon.png'
+import Logo from 'assets/images/Header/Logo.png'
+import MenuIcon from 'assets/images/Header/MenuIcon.png'
+import MenuActiveIcon from 'assets/images/Header/MenuActiveItem.png'
+
 import s from './Header.module.scss'
 
+type HeaderMenuItem = {
+  link: string
+  name: string
+}
+
+const HeaderMenuList: HeaderMenuItem[] = [
+  {
+    link: ROUTES.home,
+    name: 'Главная'
+  },
+  {
+    link: ROUTES.services,
+    name: 'Сервисы'
+  },
+  {
+    link: ROUTES.tdo,
+    name: 'Банк данных угроз'
+  }
+]
+
+type ActiveMenuItemElement = HeaderMenuItem & {
+  status: boolean
+}
 
 const Header: FC = () => {
+  const [activeElement, setActiveElement] = useState<ActiveMenuItemElement>()
+
+  const selectActiveElement = (element: HeaderMenuItem, status: boolean) => {
+    setActiveElement({
+      name: element.name,
+      link: element.link,
+      status
+    })
+  }
+
   return (
     <header className={s.header}>
-      <Link to={ROUTES.home} > Главная</Link>
-      <Link to={ROUTES.services} > Сервисы</Link>
-      <Link to={ROUTES.tdo}> Банк данных угроз</Link>
+      <div>
+        <img className={s.logo} src={Logo}></img>
+      </div>
+      <div className={s.menu}>
+        {
+          HeaderMenuList.map((item, index) => {
+            return (
+              <div className={s.menuItem} key={index}>
+                <NavLink
+                  className={({ isActive }) => {
+                    if (isActive) {
+                      selectActiveElement(item, true)
+                      return s.menuItemActive
+                    } else {
+                      return ''
+                    }
+                  }}
+                  to={item.link}>
+                    {item.name}
+                </NavLink>
+                {
+                activeElement?.status && (
+                  <div>
+                    <img className={s.menuItemActiveIcon} src={MenuActiveIcon} />
+                  </div>
+                )
+              }
+             </div>
+            )
+          })
+        }
+
+      </div>
+      <div className={s.iconsHeader}>
+        <div>
+          <img className={s.iconsHeaderItem} src={LoginIcon}></img>
+        </div>
+        <div>
+          <img className={s.iconsHeaderItem} src={MenuIcon}></img>
+        </div>
+      </div>
     </header>
   )
 }
